2c813824ad5deff4a7045fac19f150342395f98c
Takt <thanapols65@nu.ac.th>
2024-09-01 14:58:17 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:simple_app/screens/home.dart';
-import 'package:simple_app/screens/menus.dart';
+import 'package:simple_app/screens/latestRate.dart';
+import 'package:simple_app/screens/convert.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      title: "Simple App",
-      home: MyHomePage(),
-    ),
-  );
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  runApp(MaterialApp(
+    title: "Exchange Currency",
+    home: Scaffold(
       appBar: AppBar(
-        title: const Text("Simple App"),
+        title: const Text("Exchange Currency"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Menus()),
-                );
-              },
-              child: const Text('Go Menus'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Home()),
-                );
-              },
-              child: const Text('Go Homes'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+      body: const LatestRate(),
+    ),
+  ));
 }
